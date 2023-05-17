9725aaf11456ff5a89ef88a6bb98269a0b321429
mstfash <m.ashraf.hegab@gmail.com>
2023-05-17 19:42:34 +0300

    style fixes and crop image feature

--- components/MemeCreator.tsx
+++ components/MemeCreator.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import MemePreview from './MemePreview';
 import MemeEditor from './MemeEditor';
@@ -11,21 +11,29 @@ import saveAs from 'file-saver';
 export type ImageFile = File & { preview: string };
 
 const MemeCreator = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const { TextBoxContext, value } = useTextBoxConext();
   const [files, setFiles] = useState<ImageFile[]>([]);
+  const [initiateCrop, setInitiateCrop] = useState(false);
+  const childRef = useRef<any>(null);
+
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
     accept: {
       'image/*': [],
     },
     onDrop: (acceptedFiles) => {
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          })
-        )
-      );
+      setIsLoading(true);
+      if (acceptedFiles.length > 0) {
+        setFiles(
+          acceptedFiles.map((file) =>
+            Object.assign(file, {
+              preview: URL.createObjectURL(file),
+            })
+          )
+        );
+        setIsLoading(false);
+      }
     },
   });
 
@@ -42,7 +50,12 @@ const MemeCreator = () => {
       saveAs(image, 'generated-meme.png');
     }
   };
-
+  const handleSaveEdits = () => {
+    if (initiateCrop) {
+      childRef.current?.saveEdits();
+    }
+    setInitiateCrop(!initiateCrop);
+  };
   useEffect(() => {
     // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
@@ -53,58 +66,95 @@ const MemeCreator = () => {
       <section className="flex justify-between flex-col lg:flex-row">
         <div>
           <div className="lg:min-w-[600px] max-w-[600px] relative border-dashed border-2 rounded flex items-center justify-center h-[400px]">
-            <div
-              {...getRootProps({
-                className:
-                  'dropzone w-full h-full items-center justify-center flex hover:bg-gray-100 cursor-pointer',
-              })}
-            >
-              <input {...getInputProps()} />
-              <div className="max-w-[400px] p-4">
-                <div>
-                  <h3 className="text-md font-bold">
-                    Guide on how you would create a meme!
-                  </h3>
-                </div>
-                <ul className="text-sm list-disc ml-4 text-gray-500">
-                  <li className="mt-3">
-                    Drag and drop an image here, or click to select an image
-                  </li>
-                  <li className="mt-2">
-                    On the right hand side on each text box (you can add as many
-                    as you want), where you can write your text, choose color
-                    and set font size
-                  </li>
-                  <li className="mt-2">
-                    You can drag your text box to a position of your choice
-                  </li>
-                  <li className="mt-2">
-                    After you are done, click save image to download your meme
-                    with your adjustments
-                  </li>
-                </ul>
+            {files.length > 0 ? (
+              <div className="w-full h-full">
+                <MemePreview
+                  file={files[0]}
+                  initiateCrop={initiateCrop}
+                  ref={childRef}
+                />
               </div>
-            </div>
-            {files.length > 0 && (
-              <div className="mt-16">
-                <MemePreview file={files[0]} />
+            ) : (
+              <div className="w-full h-full">
+                {isLoading ? (
+                  <div>Loading...</div>
+                ) : (
+                  <div
+                    {...getRootProps({
+                      className:
+                        'dropzone p-3 py-10 w-full h-full items-center justify-center flex hover:bg-gray-100 cursor-pointer',
+                    })}
+                  >
+                    <input {...getInputProps()} onDrop={() => {}} />
+                    <div className="max-w-[400px] p-4">
+                      <div>
+                        <h3 className="text-md font-bold">
+                          Guide on how you would create a meme!
+                        </h3>
+                      </div>
+                      <ul className="text-sm list-disc ml-4 text-gray-500">
+                        <li className="mt-3">
+                          Drag and drop an image here, or click to select an
+                          image
+                        </li>
+                        <li className="mt-2">
+                          On the right hand side on each text box (you can add
+                          as many as you want), where you can write your text,
+                          choose color and set font size
+                        </li>
+                        <li className="mt-2">
+                          You can drag your text box to a position of your
+                          choice
+                        </li>
+                        <li className="mt-2">
+                          After you are done, click save image to download your
+                          meme with your adjustments
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                )}
               </div>
             )}
           </div>
           {files.length > 0 && (
             <div className="mt-3 flex items-center justify-between lg:min-w-[600px] max-w-[600px]">
-              <button
-                className="rounded border-gray-100 bg-red-600 text-white p-2 text-sm"
-                onClick={() => setFiles([])}
-              >
-                Start Over
-              </button>
-              <button
-                className="rounded border-gray-100 bg-blue-500 text-white p-2 text-sm"
-                onClick={downloadImage}
-              >
-                Save Image
-              </button>
+              <div>
+                {initiateCrop ? null : (
+                  <button
+                    className="rounded border-gray-100 bg-red-600 text-white p-2 text-sm"
+                    onClick={() => setFiles([])}
+                    disabled={initiateCrop}
+                  >
+                    Start Over
+                  </button>
+                )}
+                <button
+                  className="rounded border-gray-100 bg-green-600 text-white p-2 text-sm ml-5"
+                  onClick={handleSaveEdits}
+                  style={{ marginLeft: initiateCrop ? '0px' : '1.25rem' }}
+                >
+                  {initiateCrop ? 'Save Edits' : 'Edit Image'}
+                </button>
+                {initiateCrop ? (
+                  <button
+                    className="rounded border-gray-100 bg-gray-200 text-black p-2 text-sm ml-5"
+                    onClick={() => setInitiateCrop(!initiateCrop)}
+                  >
+                    Cancel Edit
+                  </button>
+                ) : null}
+              </div>
+
+              {initiateCrop ? null : (
+                <button
+                  className="rounded border-gray-100 bg-blue-500 text-white p-2 text-sm"
+                  onClick={downloadImage}
+                  disabled={initiateCrop}
+                >
+                  Save Image
+                </button>
+              )}
             </div>
           )}
         </div>
--- components/MemePreview.tsx
+++ components/MemePreview.tsx
@@ -1,74 +1,134 @@
 /* eslint-disable @next/next/no-img-element */
 import Draggable from 'react-draggable';
+import Cropper, { Area } from 'react-easy-crop';
 import { ImageFile } from './MemeCreator';
-import { ChangeEvent, useContext, useRef } from 'react';
+import {
+  ChangeEvent,
+  forwardRef,
+  useCallback,
+  useContext,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import useTextBoxConext from '@/hooks/useTextBoxContext';
+import { getCroppedImg } from '@/app/utils/canvasutils';
 
-const MemePreview = ({ file }: { file: ImageFile }) => {
-  const memePreviewRef = useRef<HTMLDivElement>(null);
-  const { TextBoxContext } = useTextBoxConext();
-  const { textBoxes, setTextBoxes } = useContext(TextBoxContext);
-  const handleTextChange = (
-    e: ChangeEvent<HTMLTextAreaElement>,
-    index: number
-  ) => {
-    let newState = [...textBoxes];
-    const selected = newState.find((_, i) => i === index);
-    if (selected) {
-      newState[index] = { ...selected, text: e.target.value };
-      setTextBoxes(newState);
-    }
-  };
+const MemePreview = forwardRef(
+  ({ file, initiateCrop }: { file: ImageFile; initiateCrop: boolean }, ref) => {
+    const memePreviewRef = useRef<HTMLDivElement>(null);
+    const [croppedImage, setCroppedImage] = useState<string>();
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
 
-  return (
-    <div className="absolute top-0 left-0 bg-white w-full h-full flex items-center justify-center">
-      <div
-        className="relative fit-content h-full overflow-hidden"
-        id="meme-canvas"
-        ref={memePreviewRef}
-      >
-        {textBoxes.length > 0
-          ? textBoxes.map((textBox, index) => {
-              return (
-                <Draggable
-                  defaultPosition={{ x: 50, y: 50 }}
-                  handle={`.draggable-${index}`}
-                  defaultClassName={'absolute'}
-                  bounds="parent"
-                  key={`draggable-${index}`}
-                >
-                  <div className={`group`}>
-                    <div className={`draggable-${index} relative`}>
-                      <button className="w-[100%] p-1 rounded bg-gray-200 text-sm invisible group-hover:visible">
-                        Drag here
-                      </button>
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState(1);
+    const [rotation, setRotation] = useState(0);
+    const { TextBoxContext } = useTextBoxConext();
+    const { textBoxes, setTextBoxes } = useContext(TextBoxContext);
+    const handleTextChange = (
+      e: ChangeEvent<HTMLTextAreaElement>,
+      index: number
+    ) => {
+      let newState = [...textBoxes];
+      const selected = newState.find((_, i) => i === index);
+      if (selected) {
+        newState[index] = { ...selected, text: e.target.value };
+        setTextBoxes(newState);
+      }
+    };
+    const onCropComplete = useCallback(
+      (croppedArea: Area, croppedAreaPixels: Area) => {
+        setCroppedAreaPixels(croppedAreaPixels);
+      },
+      []
+    );
+
+    const handleSaveEdits = useCallback(async () => {
+      try {
+        const croppedImage = await getCroppedImg(
+          file.preview,
+          croppedAreaPixels as Area,
+          rotation
+        );
+        console.log('donee', { croppedImage });
+        setCroppedImage(croppedImage as string);
+      } catch (e) {
+        console.error(e);
+      }
+    }, [file, croppedAreaPixels, rotation]);
+
+    useImperativeHandle(ref, () => ({
+      saveEdits() {
+        handleSaveEdits();
+      },
+    }));
+
+    return (
+      <div className="bg-white items-center w-full h-full  justify-center">
+        <div
+          className="relative w-full h-full overflow-hidden"
+          id="meme-canvas"
+          ref={memePreviewRef}
+        >
+          {textBoxes.length > 0
+            ? textBoxes.map((textBox, index) => {
+                return (
+                  <Draggable
+                    defaultPosition={{ x: 50, y: 50 }}
+                    handle={`.draggable-${index}`}
+                    defaultClassName={'absolute'}
+                    bounds="parent"
+                    key={`draggable-${index}`}
+                  >
+                    <div className={`group`}>
+                      <div className={`draggable-${index} relative`}>
+                        <button className="w-[100%] p-1 rounded bg-gray-200 text-sm invisible group-hover:visible">
+                          Drag here
+                        </button>
+                      </div>
+                      <div
+                        contentEditable
+                        suppressContentEditableWarning
+                        spellCheck={false}
+                        className={`content-editable cursor-pointer text-center max-w-[500px] rounded bg-transparent resize-none group-hover:resize  group-hover:border-gray-400 border-2 border-transparent font-extrabold`}
+                        style={{
+                          fontSize: `${textBox.fontSize}px`,
+                          color: `${textBox.color}`,
+                          lineHeight: `${textBox.fontSize}px`,
+                        }}
+                      >
+                        {textBox.text}
+                      </div>
                     </div>
-                    <div
-                      contentEditable
-                      suppressContentEditableWarning
-                      spellCheck={false}
-                      className={`content-editable cursor-pointer text-center max-w-[400px] rounded bg-transparent resize-none group-hover:resize  group-hover:border-gray-400 border-2 border-transparent font-extrabold`}
-                      style={{
-                        fontSize: `${textBox.fontSize}px`,
-                        color: `${textBox.color}`,
-                        lineHeight: `${textBox.fontSize}px`,
-                      }}
-                    >
-                      {textBox.text}
-                    </div>
-                  </div>
-                </Draggable>
-              );
-            })
-          : null}
-        <img
-          src={file.preview}
-          className="max-h-[100%] m-auto"
-          alt="image preview"
-        />
+                  </Draggable>
+                );
+              })
+            : null}
+          {initiateCrop ? (
+            <Cropper
+              image={file.preview}
+              crop={crop}
+              zoom={zoom}
+              rotation={rotation}
+              aspect={4 / 3}
+              onCropChange={setCrop}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+              onRotationChange={setRotation}
+              style={{ containerStyle: { width: '100%', height: '100%' } }}
+            />
+          ) : (
+            <img
+              src={croppedImage || file.preview}
+              className="w-full"
+              alt="image preview"
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
+MemePreview.displayName = 'MemePreview';
 export default MemePreview;
